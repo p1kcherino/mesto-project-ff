41df7e1d285e8dbf0e45890fbae27c8339ce7e39
Andrey Chaykin <chaikin.andrei@yandex.ru>
2024-05-01 16:11:00 +0300

    fix 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,23 @@
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardTemplate = document.querySelector('#card-template').content;
-
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 function createCard(item, cardRemove) {
- const card = cardTemplate.querySelector('.card').cloneNode(true);
- card.querySelector('.card__title').textContent = item.name;
- card.querySelector('.card__image').alt = item.name;
- card.querySelector('.card__image').src = item.link;
- const removeButton = card.querySelector('.card__delete-button');
- removeButton.addEventListener('click', function() {
-  cardRemove = removeCard(card);
- });
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  card.querySelector(".card__title").textContent = item.name;
+  card.querySelector(".card__image").alt = item.name;
+  card.querySelector(".card__image").src = item.link;
+  const removeButton = card.querySelector(".card__delete-button");
+  removeButton.addEventListener("click", function () {
+    cardRemove = removeCard(card);
+    function removeCard(card) {
+      card.remove();
+    }
+  });
 
- return card;
+  return card;
 }
 
-function removeCard(card){
- card.remove();
-}
-
-initialCards.forEach(function(item){
- placesList.append(createCard(item))
-})
-
-
-
-
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item));
+});
