468ea2327770357325ffd4260b76346e7b1c818e
Andrey Chaykin <chaikin.andrei@yandex.ru>
2024-05-04 12:00:12 +0300

    fix 3.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 const cardTemplate = document.querySelector("#card-template").content;
-
 const placesList = document.querySelector(".places__list");
 
 function createCard(item, cardRemove) {
   const card = cardTemplate.querySelector(".card").cloneNode(true);
-  card.querySelector(".card__title").textContent = item.name;
-  card.querySelector(".card__image").alt = item.name;
-  card.querySelector(".card__image").src = item.link;
+  const cardImage = card.querySelector(".card__image");
   const removeButton = card.querySelector(".card__delete-button");
-  removeButton.addEventListener("click", function () {
-    cardRemove = removeCard(card);
-    function removeCard(card) {
-      card.remove();
-    }
-  });
+
+  card.querySelector(".card__title").textContent = item.name;
+  cardImage.alt = item.name;
+  cardImage.src = item.link;
+  removeButton.addEventListener("click", () => cardRemove(card));
 
   return card;
 }
 
+function cardRemove(card) {
+ card.remove(); 
+  }; 
+
 initialCards.forEach(function (item) {
-  placesList.append(createCard(item));
+  placesList.append(createCard(item, cardRemove));
 });
