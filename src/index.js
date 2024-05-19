646bb3016aa5a3bcee9fefa97aced7867cd48f90
Andrey Chaykin <chaikin.andrei@yandex.ru>
2024-05-19 17:56:20 +0300

    add card
    Активна кнопка добавить, также можно добавить новую карточку

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './pages/index.css';
 import { initialCards  } from './script/cards.js';
-import { openPopup, closePopup } from './script/popap.js';
+import { openPopup, closePopup } from './script/modal.js';
 
 const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
@@ -29,11 +29,17 @@ initialCards.forEach(function (item) {
 const popupTypeEdit = document.querySelector(".popup_type_edit");
 const popupButtonClose = popupTypeEdit.querySelector(".popup__close");
 const popupForm = popupTypeEdit.querySelector(".popup__form");
-const inputName = document.querySelector(".popup__input_type_name");
-const inputDescription = document.querySelector(".popup__input_type_description");
+const inputName = popupTypeEdit.querySelector(".popup__input_type_name");
+const inputDescription = popupTypeEdit.querySelector(".popup__input_type_description");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
+
+profileEditButton.addEventListener('click', () => {
+ inputName.value = profileTitle.textContent;
+ inputDescription.value = profileDescription.textContent;
+ openPopup(popupTypeEdit); 
+});
 
 function handleFormSubmit(event){ 
  event.preventDefault();
@@ -42,15 +48,36 @@ function handleFormSubmit(event){
  closePopup(popupTypeEdit);
 }
 
-profileEditButton.addEventListener('click', () => {
- inputName.value = profileTitle.textContent;
- inputDescription.value = profileDescription.textContent;
- openPopup(popupTypeEdit); 
-});
-
 popupForm.addEventListener('submit', handleFormSubmit);
 popupButtonClose.addEventListener('click', function() {
  closePopup(popupTypeEdit);
 });
 
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupCloseTypeNewCard = popupTypeNewCard.querySelector('.popup__close');
+const popupFormNewCard = popupTypeNewCard.querySelector('.popup__form');
+const inputNameNewCard = popupTypeNewCard.querySelector('.popup__input_type_card-name');
+const inputUrlImageNewCard = popupFormNewCard.querySelector('.popup__input_type_url');
+const profileAddButtonNewCard = document.querySelector('.profile__add-button');
+
+profileAddButtonNewCard.addEventListener('click', () => {
+ openPopup(popupTypeNewCard);
+})
+
+popupCloseTypeNewCard.addEventListener('click', function() {
+ closePopup(popupTypeNewCard);
+});
+
+function addCardNew(event) {
+ event.preventDefault();
+ const newCard = {
+  link: inputUrlImageNewCard.value,
+  name: inputNameNewCard.value
+ };
+ placesList.prepend(createCard(newCard, cardRemove));
+ closePopup(popupTypeNewCard);
+ popupFormNewCard.reset();
+}
+
+popupFormNewCard.addEventListener('submit', addCardNew);
 
